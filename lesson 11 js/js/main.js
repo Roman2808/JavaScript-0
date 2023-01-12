eaa18107ee0dbbf9c75c91ef7b7861ab36657b62
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-12 15:01:47 +0900

    lesson 11 done

--- lesson 11 js/js/main.js
+++ lesson 11 js/js/main.js
@@ -1,6 +1,6 @@
 // тому что слева мы присваиваем то что справа
 var firstNameId = "first-name";
-// = это оператов присваивания... то есть то что слева (переменная) ей присваивается значение справа
+// = это оператор присваивания... то есть то что слева (переменная) ей присваивается значение справа
 var firstNameEl = document.getElementById(firstNameId);
 var lastNameId = "last-name";
 var lastNameEl = document.getElementById(lastNameId);
@@ -15,17 +15,45 @@ var avatarWrapperEl = document.getElementById(avatarWrapperId);
 var avatarId = "avatar";
 var avatarEl = document.getElementById(avatarId);
 
+debugger;
+function alertValue() {
+    console.log(firstNameEl.value);
+    console.log(lastNameEl.value);
+    console.log(addressEl.value);
+    console.log(citiesEl.value);
+}
+
 // если тому что слева присваивается то что справа от оператора присваивания то
 // это значит что firstNameEl.value приобретет новое значение Лана
 firstNameEl.value = "Lana";
+
+console.log(firstNameEl.value);
+console.log(lastNameEl.value);
+console.log(addressEl.value);
+
+alertValue();
+
 // lastNameEl.value = "Egorova";
 lastNameEl.className = "last-name-input default-input error-input";
+
+alertValue();
+
 lastNameEl.title = "Wrong last name";
-avatarEl.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK__zgU7_Eiudx6weoFKV9inm8aANKqHSp8g&usqp=CAU";
+
+alertValue();
+
+avatarEl.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK__zgU7_Eiudx6weoFKV9inm8aANKqHSp8g&usqp=CAU";
+
+alertValue();
 
 avatarEl.title = "I am avatar";
 
+alertValue();
+
 citiesEl.value = "Hordogoy";
+
+alertValue();
+
 hobbiesEl.value = "I have not interest";
 
 avatarWrapperEl.innerHTML = "<ul id='list'><li>1</li><li>2</li></ul>";
@@ -33,4 +61,4 @@ avatarWrapperEl.innerHTML = "<ul id='list'><li>1</li><li>2</li></ul>";
 var listId = "list";
 var numberList = document.getElementById(listId);
 
-window.alert(numberList.innerHTML);
+console.log(numberList.innerHTML);
