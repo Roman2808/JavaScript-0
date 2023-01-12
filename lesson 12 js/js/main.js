01c78e17b11eb82b4d2b8ee8c1b44a0599a5561c
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-12 16:41:41 +0900

    lesson 12 js done

--- lesson 12 js/js/main.js
+++ lesson 12 js/js/main.js
@@ -1,46 +1,28 @@
-function alertFirstNameValues() {
-    console.log(firstNameEl.value);
-    console.log(firstNameEl.className);
+function alertValues(elementParameter) {
+    console.log(elementParameter.value);
+    console.log(elementParameter.className);
 }
 
-function alertLastNameValues() {
-    console.log(lastNameEl.value);
-    console.log(lastNameEl.classAame);
-}
-
-function alertAddressValues() {
-    console.log(addressEl.value);
-    console.log(addressEl.className)
-}
-
-function alertHobbiesValues() {
-    console.log(hobbiesEl.value);
-    console.log(hobbiesEl.className);
-}
-
-
-// тому что слева мы присваиваем то что справа
 var firstNameId = "first-name";
-// = это оператор присваивания... то есть то что слева (переменная) ей присваивается значение справа
 var firstNameEl = document.getElementById(firstNameId);
-alertFirstNameValues();
+// alertFirstNameValues();
+alertValues(firstNameEl);
 
 var lastNameId = "last-name";
 var lastNameEl = document.getElementById(lastNameId);
-alertLastNameValues();
+alertValues(lastNameEl);
 
 var addressId = "address";
 var addressEl = document.getElementById(addressId);
-alertAddressValues()
+alertValues(addressEl )
 
 var citiesId = "cities";
 var citiesEl = document.getElementById(citiesId);
-// console.log(citiesEl.value);
-// console.log(citiesEl.className);
+alertValues(citiesEl);
 
 var hobbiesId = "hobbies";
 var hobbiesEl = document.getElementById(hobbiesId);
-alertHobbiessValues()
+alertValues(hobbiesEl)
 
 var avatarWrapperId = "avatar-wrapper";
 var avatarWrapperEl = document.getElementById(avatarWrapperId);
@@ -50,21 +32,21 @@ var avatarEl = document.getElementById(avatarId);
 // если тому что слева присваивается то что справа от оператора присваивания то
 // это значит что firstNameEl.value приобретет новое значение Лана
 firstNameEl.value = "Lana";
-console.log(firstNameEl.value);
-console.log(firstNameEl.className);
+// console.log(firstNameEl.value);
+// console.log(firstNameEl.className);
 
 // lastNameEl.value = "Egorova";
 lastNameEl.className = "last-name-input default-input error-input";
-console.log(lastNameEl.value);
-console.log(lastNameEl.className);
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
 
 lastNameEl.title = "Wrong last name";
-console.log(lastNameEl.value);
-console.log(lastNameEl.className);
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
 
 firstNameEl.className = "new-class";
-console.log(firstNameEl.value);
-console.log(firstNameEl.className);
+// console.log(firstNameEl.value);
+// console.log(firstNameEl.className);
 
 avatarEl.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK__zgU7_Eiudx6weoFKV9inm8aANKqHSp8g&usqp=CAU";
 
