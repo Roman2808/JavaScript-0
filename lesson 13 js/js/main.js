127870fe75545406c445a9eb61e78ebde8f69cea
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-13 16:39:03 +0900

    l13js

--- lesson 13 js/js/main.js
+++ lesson 13 js/js/main.js
@@ -1,38 +1,69 @@
-// это то же самое что
-// function firstNameLog() {
-//   var id = "first-name";
-//   var el = document.getElementById(id);
-//   console.log(el.value);
-// }
-
-// function firstNameLog() {
-//   var id = "last-name";
-//   var el = document.getElementById(id);
-//   console.log(el.value);
-// }
-// что и это
-function logValue(id) {
-  var el = document.getElementById(id);
-  console.log(el.value);
+function alertValues(elementParameter) {
+    // console.log(elementParameter.value);
+    // console.log(elementParameter.className);
 }
 
-function getValue(id) {
-  var el = document.getElementById(id);
-  return el.value;
+function getEl(id) {
+    var el = document.getElementById(id);
+    return el;
 }
-
 
 var firstNameId = "first-name";
+var firstNameEl =  getEl(firstNameId);
+// alertFirstNameValues();
+alertValues(firstNameEl);
+
 var lastNameId = "last-name";
+var lastNameEl =  getEl(lastNameId);
+alertValues(lastNameEl);
 
-logValue(firstNameId);
-logValue(firstNameId);
-logValue(firstNameId);
+var addressId = "address";
+var addressEl =  getEl(addressId);
+alertValues(addressEl )
 
-logValue(lastNameId);
-logValue(lastNameId);
-logValue(lastNameId);
+var citiesId = "cities";
+var citiesEl =  getEl(citiesId);
+alertValues(citiesEl);
 
-var value = getValue(lastNameId);
+var hobbiesId = "hobbies";
+var hobbiesEl =  getEl(hobbiesId);
+alertValues(hobbiesEl)
 
-window.alert(value);
+var avatarWrapperId = "avatar-wrapper";
+var avatarWrapperEl =  getEl(avatarWrapperId);
+var avatarId = "avatar";
+var avatarEl =  getEl(avatarId);
+
+// если тому что слева присваивается то что справа от оператора присваивания то
+// это значит что firstNameEl.value приобретет новое значение Лана
+firstNameEl.value = "Lana";
+// console.log(firstNameEl.value);
+// console.log(firstNameEl.className);
+
+// lastNameEl.value = "Egorova";
+lastNameEl.className = "last-name-input default-input error-input";
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
+
+lastNameEl.title = "Wrong last name";
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
+
+firstNameEl.className = "new-class";
+// console.log(firstNameEl.value);
+// console.log(firstNameEl.className);
+
+avatarEl.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK__zgU7_Eiudx6weoFKV9inm8aANKqHSp8g&usqp=CAU";
+
+avatarEl.title = "I am avatar";
+
+citiesEl.value = "Hordogoy";
+
+hobbiesEl.value = "I have not interest";
+
+avatarWrapperEl.innerHTML = "<ul id='list'><li>1</li><li>2</li></ul>";
+
+var listId = "list";
+var numberList = document.getElementById(listId);
+
+// console.log(numberList.innerHTML);
