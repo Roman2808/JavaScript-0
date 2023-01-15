12ca5a6d57df1332b9a5694bfccb81dfdbf45cf8
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-15 17:37:25 +0900

    lesson 14 js done

--- lesson 14 js/js/main.js
+++ lesson 14 js/js/main.js
@@ -4,7 +4,7 @@ function addErrorClass(elementId) {
 }
 
 function addErrorClassToAllInputs() {
-    debugger;
+    // debugger;
     addErrorClass("first-name");
     addErrorClass("last-name");
     addErrorClass("address");
@@ -12,9 +12,23 @@ function addErrorClassToAllInputs() {
 
 // addErrorClass("last-name");
 
-window.setTimeout(addErrorClassToAllInputs, 2000);
+// window.setTimeout(addErrorClassToAllInputs, 2000);
 
 
 var sendButton = document.getElementById("send-button");
 
-sendButton.addEventListener("click", addErrorClassToAllInputs);
+// addErrorClass();
+
+sendButton.addEventListener("click", onSendButtonClick);
+
+function onSendButtonClick() {
+    addErrorClass("last-name");
+}
+
+function onFirstNameKeyup() {
+    addErrorClass("first-name");
+}
+
+var firstNameEl = document.getElementById("first-name");
+
+firstNameEl.addEventListener("keyup", onFirstNameKeyup);
