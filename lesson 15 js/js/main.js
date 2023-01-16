1233d344d8166aa0a3569842ab3862f064be0fac
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-16 14:16:48 +0900

    lesson 15 js done

--- lesson 15 js/js/main.js
+++ lesson 15 js/js/main.js
@@ -2,3 +2,52 @@ var buttonPlus = document.getElementById("buttonPlus");
 var buttonMinus = document.getElementById("buttonMinus");
 var buttonMultiplay = document.getElementById("buttonMultiplay");
 var buttonDevide = document.getElementById("buttonDevide");
+
+function onButtonPlusClick() {
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var summ = number1 + number2;
+    window.alert(summ);
+}
+
+function onButtonMinusClick() {
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var summ = number1 - number2;
+    window.alert(summ);
+}
+
+function onButtonMultiplayClick() {
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var summ = number1 * number2;
+    window.alert(summ);
+}
+
+function onButtonDevideClick() {
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var summ = number1 / number2;
+    window.alert(summ);
+}
+
+buttonPlus.addEventListener("click", onButtonPlusClick);
+buttonMinus.addEventListener("click", onButtonMinusClick);
+buttonMultiplay.addEventListener("click", onButtonMultiplayClick);
+buttonDevide.addEventListener("click", onButtonDevideClick);
