2411a8fd6ee7bac678f0d848e0f4cd179e256f50
Роман <117072703+Roman2808@users.noreply.github.com>
2023-01-16 13:47:50 +0900

    l15 js

--- lesson 15 js/js/main.js
+++ lesson 15 js/js/main.js
@@ -1,34 +1,4 @@
-function addErrorClass(elementId) {
-    var element = document.getElementById(elementId);
-    element.className = "error-input";
-}
-
-function addErrorClassToAllInputs() {
-    // debugger;
-    addErrorClass("first-name");
-    addErrorClass("last-name");
-    addErrorClass("address");
-}
-
-// addErrorClass("last-name");
-
-// window.setTimeout(addErrorClassToAllInputs, 2000);
-
-
-var sendButton = document.getElementById("send-button");
-
-// addErrorClass();
-
-sendButton.addEventListener("click", onSendButtonClick);
-
-function onSendButtonClick() {
-    addErrorClass("last-name");
-}
-
-function onFirstNameKeyup() {
-    addErrorClass("first-name");
-}
-
-var firstNameEl = document.getElementById("first-name");
-
-firstNameEl.addEventListener("keyup", onFirstNameKeyup);
+var buttonPlus = document.getElementById("buttonPlus");
+var buttonMinus = document.getElementById("buttonMinus");
+var buttonMultiplay = document.getElementById("buttonMultiplay");
+var buttonDevide = document.getElementById("buttonDevide");
