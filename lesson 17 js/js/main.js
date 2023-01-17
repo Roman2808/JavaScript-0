6d533e7cad5712aeeec6616e0115e9a5148bf357
Роман <r_o_m_a_91@mail.ru>
2023-01-17 13:01:00 +0900

    l17js

--- lesson 17 js/js/main.js
+++ lesson 17 js/js/main.js
@@ -6,23 +6,18 @@ var buttonDevide = document.getElementById("buttonDevide");
 var input1 = document.getElementById("number1");
 var input2 = document.getElementById("number2");
 
-function getNumber1(){
-    return Number(input1.value);
-}
-
-function getNumber2(){
-    return Number(input2.value);
-}
-
 function makeOperation(operationCode) {
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
     if (operationCode === "+") {
-        var result = getNumber1() + getNumber2();
+        var result = number1 + number2;
     } else if (operationCode === "-") {
-        var result = getNumber1() - getNumber2();
+        var result = number1 - number2;
     } else if (operationCode === "*") {
-        var result = getNumber1() * getNumber2();
+        var result = number1 * number2;
     } else if (operationCode === "/") {
-        var result = getNumber1() / getNumber2();
+        var result = number1 / number2;
     } else {
         window.alert("operation is unknown");
     }
