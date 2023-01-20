84e250474adbd54f3e7b24657316ea25e9c6c3e2
Роман <r_o_m_a_91@mail.ru>
2023-01-20 15:25:13 +0900

    l22

---
+++ lesson 22/js/car.js
@@ -0,0 +1,16 @@
+var car1 = {
+  name: "Toyota Wish",
+  isTurnOn:false,
+  speed: 0,
+  engine: {
+    v: 1.8,
+    horsesPower: 167
+  },
+  start: function() {
+    car1.isTurnOn = true;
+    car1.speed = 10;
+  }
+};
+
+
+
