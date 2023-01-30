bb1514798059b5eeba1902cd2efa9a04f4faaa38
Роман <r_o_m_a_91@mail.ru>
2023-01-30 09:46:54 +0900

    l28

--- lesson 28/js/car.js
+++ lesson 28/js/car.js
@@ -1,13 +1,14 @@
-var car1 = {
-  name: "Toyota Wish",
-  isTurnOn:false,
-  speed: 0,
-  engine: {
-    v: 1.8,
+function Car(name) {
+  this.name = "name";
+  this.isTurnOn =false;
+  this.speed = 0;
+  this.engine = {
+    v: 1.8;
     horsesPower: 167,
     isTurnOn: false
-  },
-  start: function() {
+  };
+
+  this.start = function() {
     this.isTurnOn = true;
     this.speed = 10;
     this.engine.isTurnOn = true;
--- lesson 28/js/index.js
+++ lesson 28/js/index.js
@@ -1,5 +1,11 @@
-var slider1 = new Slider();
-var slider2 = new Slider();
+// var slider1 = new Slider();
+// var slider2 = new Slider();
 
-slider1.start("itk-slider1");
-slider2.start("itk-slider2");
+// slider1.start("itk-slider1");
+// slider2.start("itk-slider2");
+
+var wish = new Car("wish");
+wish.start();
+var filder = new Car("filder");
+filder.start();
+
