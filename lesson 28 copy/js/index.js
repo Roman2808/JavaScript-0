e334002333c39562a1b0e91623b28be20b8e7ed7
Роман <che_lana13@mail.ru>
2023-01-31 17:58:55 +0900

    28

---
+++ lesson 28 copy/js/index.js
@@ -0,0 +1,13 @@
+// var slider1 = new Slider();
+// var slider2 = new Slider();
+
+// slider1.start("itk-slider1");
+// slider2.start("itk-slider2");
+
+var wish = new Car("wish");
+wish.start();
+var filder = new Car("filder");
+filder.start();
+var lx570 = new Car("lx570");
+lx570.start();
+
